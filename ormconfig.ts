8eeca66c56d2918dc3d413714edf5d053feaed01
Thiago Trindade <thiago.trindadexd@gmail.com>
2021-02-23 12:01:55 -0300

    migration CreateUsers

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,4 +1,8 @@
 export default {
   type: process.env.TYPEORM_CONNECTION,
-  database: process.env.TYPEORM_DATABASE
+  database: process.env.TYPEORM_DATABASE,
+  migrations: [process.env.TYPEORM_MIGRATIONS],
+  cli: {
+    migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR
+  }
 }
