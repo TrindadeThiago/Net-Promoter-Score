cc647bca5fcd2c5a5891f73c6a1017c4516b9470
Thiago Trindade <thiago.trindadexd@gmail.com>
2021-02-24 21:19:31 -0300

    create repositories and surveys controller and model

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express'
+import { SurveysController } from './controllers/SurveysController'
 
-import { UserController } from './controllers/UserController'
+import { UsersController } from './controllers/UsersController'
 
 const routes = Router()
 
-const userController = new UserController()
+const userController = new UsersController()
+const serveysController = new SurveysController()
 
+// users routes
 routes.post('/users', userController.create)
+
+// surveys routes
+routes.get('/surveys', serveysController.show)
+routes.post('/surveys', serveysController.create)
 
 export default routes
