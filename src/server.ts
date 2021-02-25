e2323dfdf15fda9b0c3e625d01f0efafd340b561
Thiago Trindade <thiago.trindadexd@gmail.com>
2021-02-24 22:23:02 -0300

    refactory server.ts and created app.ts and implamentation automatized test

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,3 @@
-import 'reflect-metadata'
+import { app } from "./app";
 
-import express from 'express'
-
-import routes from './routes'
-
-import './database'
-
-const server = express()
-
-server.use(express.json())
-server.use(routes)
-
-
-server.listen(3333, () => console.log("Server is running!"))
+app.listen(3333, () => console.log("Server is running!"))
